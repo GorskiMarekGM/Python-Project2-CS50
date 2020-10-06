8c574034ee436f5f20ccf0ca067553cee00c25fc
marekgorskiwsb@gmail.com <marekgorskiwsb@gmail.com>
2020-10-06 13:16:22 +0200

    models update

--- auctions/admin.py
+++ auctions/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from .models import Bid, Auction, Comment, Category, User
 # Register your models here.
+
+admin.site.register(Bid)
+admin.site.register(Auction)
+admin.site.register(Comment)
+admin.site.register(Category)
+admin.site.register(User)
--- auctions/models.py
+++ auctions/models.py
@@ -4,3 +4,25 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+class Category(models.Model):
+    name = models.CharField(max_length=64)
+
+class Auction(models.Model):
+    name = models.CharField(max_length=64)
+    price = models.IntegerField(max_length=10)
+    creation_date = models.TimeField()
+    auction_category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="categories")
+
+    def __str__(self):
+        return f"{self.id}"
+
+class Bid(models.Model):
+    price = models.IntegerField()
+    auction_bid = models.ForeignKey(Auction, on_delete=models.CASCADE, related_name="bids")
+    auctioner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="auctioner")
+
+class Comment(models.Model):
+    commenter = models.ForeignKey(User, on_delete=models.CASCADE, related_name="commenter")
+    comment_to = models.ForeignKey(Auction, on_delete=models.CASCADE, related_name="auction")
+    text = models.TextField()
